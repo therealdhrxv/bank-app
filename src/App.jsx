de9ca91b27358d27f9d3378d88da30fef115fd26
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-03-04 02:30:18 +0530

    Many component's skeleton made

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import styles from './style'
 
 const App = () => {
   return (
+
+    // The navbar section
     <div className='bg-indigo-600 text-center w-48'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`} >
         <div className={`${styles.boxWidth}`}>
@@ -10,6 +12,28 @@ const App = () => {
           Navbar
         </div>
       </div>
+
+      {/* The hero section */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      {/* The hero section */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Business 
+          Billing
+          CardDeal
+          Testimonial
+          Clients
+          CTA
+          Footer
+        </div>
+      </div>
+
     </div>
   )
 }
