01dfdef72148acb8c51cdadef3c2329f65566107
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-03-04 02:18:05 +0530

    Added the styles.js and layed the navbar layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styles from './style' 
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='bg-indigo-600 text-center w-48'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`} >
+        <div className={`${styles.boxWidth}`}>
+          {/* Here we'll render our navbar */}
+          Navbar
+        </div>
+      </div>
     </div>
   )
 }
