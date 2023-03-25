b736e9f8c62b0df8d2967e9407eb7dc40fc61399
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-03-26 01:15:04 +0530

    Added the rest of the components

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          <Stats />
+          {/* <Stats /> */}
           <Business  />
           <Billing />
           <CardDeal />
